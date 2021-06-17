e32a88c9dfb70df954c7e44c9044097bb035efc8
Kyle <kyleyoung.9210@gmail.com>
2021-06-16 20:47:57 -0400

    added react router dom routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
+import Container from '@material-ui/core/Container';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Saved from './Saved/index';
+import Search from './Search/index';
 
 function App() {
 	return (
-		<div className="App">
-			<div className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h2>Welcome to React</h2>
+		<Router>
+			<div>
+				<Nav />
+				<Container>
+					<Switch>
+						<Route exact path={['/', '/search']} component={Search} />
+						<Route exact path="/saved" component={Saved} />
+					</Switch>
+				</Container>
 			</div>
-			<p className="App-intro">
-				To get started, edit <code>src/App.js</code> and save to reload.
-			</p>
-		</div>
+		</Router>
 	);
 }
 
