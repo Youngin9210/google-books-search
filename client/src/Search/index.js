27a5410806a4d719c6ad671166685c1f30a915a7
Kyle <kyleyoung.9210@gmail.com>
2021-06-17 13:33:01 -0400

    created results structure

--- client/src/Search/index.js
+++ client/src/Search/index.js
@@ -1,13 +1,14 @@
-import { Button, TextField, Typography } from '@material-ui/core';
+import { Button, Container, TextField, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
-	container: {
+	headerContainer: {
 		padding: '2rem 0',
 		width: '100%',
 		textAlign: 'center',
-		background: 'white',
+		background: 'lightgrey',
+		margin: '2rem 0',
 	},
 	h6: {
 		fontSize: '1.75rem',
@@ -19,9 +20,10 @@ const useStyles = makeStyles((theme) => ({
 	searchContainer: {
 		padding: '2rem',
 		background: 'lightgrey',
-		color: 'white',
+		color: 'black',
 		display: 'flex',
 		flexWrap: 'wrap',
+		margin: '2rem 0',
 	},
 	searchInput: {
 		margin: '1.5rem 0',
@@ -29,13 +31,42 @@ const useStyles = makeStyles((theme) => ({
 	searchButton: {
 		marginLeft: 'auto',
 	},
+	resultsContainer: {
+		padding: '2rem',
+		background: 'lightgrey',
+		color: 'black',
+		display: 'flex',
+		flexWrap: 'wrap',
+		margin: '2rem 0',
+	},
+	resultButton: {
+		margin: '.25rem',
+	},
+	bookContainer: {
+		border: '1px solid black',
+		padding: '1rem',
+		margin: '1rem 0',
+	},
+	bookHeader: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		marginBottom: '1rem',
+	},
+	bookDetails: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	bookSummary: {
+		padding: '1rem',
+	},
 }));
 
 export default function Search() {
 	const classes = useStyles();
 	return (
 		<>
-			<div id="searchHeaderContainer" className={classes.container}>
+			<div id="searchHeaderContainer" className={classes.headerContainer}>
 				<Typography
 					id="searchHeader"
 					variant="h4"
@@ -74,6 +105,56 @@ export default function Search() {
 				>
 					Search
 				</Button>
+			</div>
+			<div className={classes.resultsContainer}>
+				<Typography variant="h5">Results</Typography>
+				<Container className={classes.bookContainer}>
+					<div className={classes.bookHeader}>
+						<div>
+							<Typography variant="h5">Book Title</Typography>
+							<Typography variant="caption">Brief description</Typography>
+							<Typography variant="body1">Written By: John Doe</Typography>
+						</div>
+						<div>
+							<Button
+								variant="outlined"
+								color="secondary"
+								className={classes.resultButton}
+							>
+								View
+							</Button>
+							<Button
+								variant="outlined"
+								color="secondary"
+								className={classes.resultButton}
+							>
+								Save
+							</Button>
+						</div>
+					</div>
+					<div className={classes.bookDetails}>
+						<div>
+							<img src="https://via.placeholder.com/200" alt="" />
+						</div>
+						<div>
+							<Typography variant="body2" className={classes.bookSummary}>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut,
+								deserunt molestias veritatis corrupti sequi nulla, ipsum
+								molestiae suscipit porro nemo asperiores laborum, id illo.
+								Architecto, modi error. Perspiciatis dicta tempora voluptas
+								accusamus veritatis laudantium eum repellat, vero cumque? Minima
+								iusto modi rem possimus similique facere tenetur veritatis
+								accusamus, eveniet excepturi aut. Error atque laborum quos? Unde
+								laudantium non ducimus quos consequatur deserunt sed nisi
+								deleniti, molestias quod cum quidem eaque, nostrum dolorem
+								incidunt esse. Quaerat ab nesciunt voluptas enim quae excepturi,
+								quibusdam sequi quod porro neque ea dolor. Corporis culpa velit
+								voluptatum cumque voluptate nemo ea magnam voluptates dolorum
+								eum!
+							</Typography>
+						</div>
+					</div>
+				</Container>
 			</div>
 		</>
 	);
