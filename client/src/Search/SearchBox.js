84bea0bbcc732ec9e5e60e55ccc913932eb6020e
Kyle <kyleyoung.9210@gmail.com>
2021-06-19 10:00:27 -0400

    passing props

--- client/src/Search/SearchBox.js
+++ client/src/Search/SearchBox.js
@@ -1,7 +1,21 @@
 import { Button, TextField, Typography } from '@material-ui/core';
 import React from 'react';
 
-export default function SearchBox({ classes }) {
+export default function SearchBox({
+	classes,
+	onChange,
+	onSubmit,
+	onKeyUp,
+	value,
+}) {
+	// const searchInput = useRef(null);
+	// const searchBtn = useRef(null);
+
+	// useEffect(() => {
+	// 	searchInput.current.addEventListener('keydown', onKeyDown);
+	// 	searchBtn.current.addEventListener('click', onSubmit);
+	// });
+
 	return (
 		<div className={classes.searchContainer}>
 			<Typography variant="subtitle1">Book Search</Typography>
@@ -12,16 +26,24 @@ export default function SearchBox({ classes }) {
 				placeholder="Harry Potter"
 				color="secondary"
 				fullWidth
+				type="search"
 				margin="normal"
 				InputLabelProps={{
 					shrink: true,
 				}}
 				variant="outlined"
+				onChange={onChange}
+				onKeyUp={onKeyUp}
+				value={value}
+				// ref={searchInput}
 			/>
 			<Button
+				type="submit"
 				color="secondary"
 				variant="outlined"
 				className={classes.searchButton}
+				onClick={onSubmit}
+				// ref={searchBtn}
 			>
 				Search
 			</Button>
