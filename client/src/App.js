4deae139ccf9a763bbac75084fba068aecab656c
Kyle <kyleyoung.9210@gmail.com>
2021-06-22 16:02:56 -0400

    removed useEffect and currentPage state and placed into results container

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import 'fontsource-roboto';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Nav from './components/Nav';
@@ -7,14 +7,6 @@ import Saved from './pages/Saved';
 import Search from './pages/Search';
 
 function App() {
-	const [currentPage, setCurrentPage] = useState('');
-	useEffect(() => {
-		window.location.pathname === '/'
-			? setCurrentPage('search')
-			: setCurrentPage('saved');
-		console.log(currentPage);
-		return;
-	}, [currentPage]);
 	return (
 		<Router>
 			<div>
@@ -22,18 +14,8 @@ function App() {
 				<Header />
 				<div>
 					<Switch>
-						<Route
-							exact
-							path="/"
-							component={Search}
-							currentPage={currentPage}
-						/>
-						<Route
-							exact
-							path="/saved"
-							component={Saved}
-							currentPage={currentPage}
-						/>
+						<Route exact path="/" component={Search} />
+						<Route exact path="/saved" component={Saved} />
 					</Switch>
 				</div>
 			</div>
