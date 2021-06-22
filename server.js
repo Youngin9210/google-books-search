38216055ae8a1c5ebfb8f674a887821313372077
Kyle <kyleyoung.9210@gmail.com>
2021-06-22 13:55:41 -0400

    added requirements for mongodb atlas connection

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
@@ -16,7 +18,16 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+	// allowing app to fall back to old parser incase of bug finds
+	useNewUrlParser: true,
+	// removes connection options that are no longer relevant with new topology engine
+	useUnifiedTopology: true,
+	// using mongoose default engine build
+	useCreateIndex: true,
+	// using native findOneAndUpdate()
+	useFindAndModify: false,
+});
 
 // Start the API server
 app.listen(PORT, function () {
