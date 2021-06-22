512171c9f9c677a8a6b01eb34374c48a3073fd84
Kyle <kyleyoung.9210@gmail.com>
2021-06-22 14:29:38 -0400

    extracted card content into it's own component

---
+++ client/src/components/BookCard.js
@@ -0,0 +1,63 @@
+import { Button, Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function BookCard({
+	id,
+	title,
+	publishedDate,
+	description,
+	authors,
+	imageLinks,
+	previewLink,
+	classes,
+	handleSave,
+	handleDelete,
+	currentPage,
+}) {
+	return (
+		<div className={classes.bookContainer}>
+			<div className={classes.bookHeader}>
+				<div>
+					<Typography variant="h6">{title}</Typography>
+					<Typography variant="caption">
+						Published Date: {publishedDate}
+					</Typography>
+					<Typography variant="body1">Authors: {authors}</Typography>
+				</div>
+				<div>
+					<Button
+						variant="outlined"
+						color="secondary"
+						className={classes.resultButton}
+						href={previewLink}
+						target="_blank"
+						data-id={id}
+					>
+						View
+					</Button>
+					<Button
+						variant="outlined"
+						color="secondary"
+						className={classes.resultButton}
+						id={id}
+						onClick={() =>
+							currentPage === 'search' ? handleSave(id) : handleDelete(id)
+						}
+					>
+						{currentPage === 'search' ? 'Save' : 'Delete'}
+					</Button>
+				</div>
+			</div>
+			<div className={classes.bookDetails}>
+				<div>
+					<img src={imageLinks.thumbnail} alt="" />
+				</div>
+				<div>
+					<Typography variant="body2" className={classes.bookSummary}>
+						{description}
+					</Typography>
+				</div>
+			</div>
+		</div>
+	);
+}
