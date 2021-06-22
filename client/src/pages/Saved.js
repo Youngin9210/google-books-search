37da2feea4b63018315fbee9f233a211e6c65a65
Kyle <kyleyoung.9210@gmail.com>
2021-06-22 14:33:04 -0400

    moved saved component into pages dir. created getBooks method and a method to handle deleting books from saved list

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import useStyles from '../assets/useStyles';
+import ResultsContainer from '../components/ResultsContainer';
+import API from '../utils/API';
+
+export default function Saved({ currentPage }) {
+	const [savedBooks, setSavedBooks] = useState([]);
+	useEffect(() => {
+		getBooks();
+		return;
+	}, []);
+
+	const getBooks = () => {
+		API.getSavedBooks()
+			.then((res) => setSavedBooks(res.data))
+			.catch((err) => console.log(err.message));
+	};
+
+	const handleDelete = (bookId) => {
+		console.log('bookId:', bookId);
+		API.deleteBook(bookId).then(() => console.log('deleted'));
+	};
+
+	const classes = useStyles();
+	return (
+		<>
+			<ResultsContainer
+				classes={classes}
+				results={savedBooks}
+				currentPage={currentPage}
+				handleDelete={handleDelete}
+			/>
+		</>
+	);
+}
