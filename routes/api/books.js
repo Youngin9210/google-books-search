b7840fa4c7f8bb9856d4802e2c0b225c47a63cf0
Kyle <kyleyoung.9210@gmail.com>
2021-06-22 14:02:11 -0400

    removed uneccessary put route

--- routes/api/books.js
+++ routes/api/books.js
@@ -8,7 +8,6 @@ router.route('/').get(booksController.findAll).post(booksController.create);
 router
 	.route('/:id')
 	.get(booksController.findById)
-	.put(booksController.update)
 	.delete(booksController.remove);
 
 module.exports = router;
